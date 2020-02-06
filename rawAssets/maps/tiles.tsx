b60c1385f709e7fd732773ea3b6aa3c4d5b23a7d
Ryan Malm <ryan.malm@gmail.com>
2020-02-06 10:27:14 -0600

    new tile art, working out new flipspace bugs

--- rawAssets/maps/tiles.tsx
+++ rawAssets/maps/tiles.tsx
@@ -1,4 +1,100 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="2019.12.11" name="tiles" tilewidth="8" tileheight="8" tilecount="256" columns="16">
- <image source="../../src/img/tiles.png" width="128" height="128"/>
+<tileset version="1.2" tiledversion="2019.12.11" name="tiles" tilewidth="8" tileheight="8" tilecount="4096" columns="16">
+ <image source="../../src/img/tiles.png" width="128" height="2048"/>
+ <terraintypes>
+  <terrain name="bgLabWallPanel" tile="224"/>
+  <terrain name="dirt1" tile="1063"/>
+  <terrain name="dirtWall" tile="0"/>
+ </terraintypes>
+ <tile id="148" terrain="2,2,2,2"/>
+ <tile id="149" terrain="2,2,2,2"/>
+ <tile id="164" terrain="2,2,2,2"/>
+ <tile id="180" terrain="2,2,2,2"/>
+ <tile id="225" terrain=",,,0"/>
+ <tile id="226" terrain=",,0,0"/>
+ <tile id="227" terrain=",,0,"/>
+ <tile id="240" terrain=",,,0"/>
+ <tile id="241" terrain=",0,0,0"/>
+ <tile id="243" terrain="0,,0,0"/>
+ <tile id="244" terrain=",,0,"/>
+ <tile id="256" terrain=",0,,0"/>
+ <tile id="258" terrain="0,0,0,0"/>
+ <tile id="260" terrain="0,,0,"/>
+ <tile id="272" terrain=",0,,"/>
+ <tile id="273" terrain="0,0,,0"/>
+ <tile id="275" terrain="0,0,0,"/>
+ <tile id="276" terrain="0,,,"/>
+ <tile id="289" terrain=",0,,"/>
+ <tile id="290" terrain="0,0,,"/>
+ <tile id="291" terrain="0,,,"/>
+ <tile id="304" terrain=",2,2,2"/>
+ <tile id="305" terrain=",,,2"/>
+ <tile id="306" terrain=",,2,2"/>
+ <tile id="307" terrain=",,2,"/>
+ <tile id="308" terrain="2,,2,2"/>
+ <tile id="309" terrain=",,2,2"/>
+ <tile id="310" terrain=",,2,2"/>
+ <tile id="320" terrain=",,,2"/>
+ <tile id="321" terrain=",2,2,2"/>
+ <tile id="322" terrain="2,2,2,2"/>
+ <tile id="323" terrain="2,,2,2"/>
+ <tile id="324" terrain=",,2,"/>
+ <tile id="325" terrain="2,2,,"/>
+ <tile id="326" terrain="2,2,,"/>
+ <tile id="336" terrain=",2,,2"/>
+ <tile id="337" terrain="2,2,2,2"/>
+ <tile id="338" terrain="2,2,2,2"/>
+ <tile id="339" terrain="2,2,2,2"/>
+ <tile id="340" terrain="2,,2,"/>
+ <tile id="341" terrain=",2,,2"/>
+ <tile id="342" terrain="2,,2,"/>
+ <tile id="352" terrain=",2,,"/>
+ <tile id="353" terrain="2,2,,2"/>
+ <tile id="354" terrain="2,2,2,2"/>
+ <tile id="355" terrain="2,2,2,"/>
+ <tile id="356" terrain="2,,,"/>
+ <tile id="357" terrain=",2,,2"/>
+ <tile id="358" terrain="2,,2,"/>
+ <tile id="368" terrain="2,2,,2"/>
+ <tile id="369" terrain=",2,,"/>
+ <tile id="370" terrain="2,2,,"/>
+ <tile id="371" terrain="2,,,"/>
+ <tile id="372" terrain="2,2,2,"/>
+ <tile id="1111" terrain=",,,1"/>
+ <tile id="1112" terrain=",,1,1"/>
+ <tile id="1113" terrain=",,1,"/>
+ <tile id="1126" terrain=",,,1"/>
+ <tile id="1127" terrain=",1,1,1"/>
+ <tile id="1128" terrain="1,1,1,1"/>
+ <tile id="1129" terrain="1,,1,1"/>
+ <tile id="1130" terrain=",,1,"/>
+ <tile id="1142" terrain=",1,,1"/>
+ <tile id="1143" terrain="1,1,1,1"/>
+ <tile id="1144" terrain="1,1,1,1"/>
+ <tile id="1145" terrain="1,1,1,1"/>
+ <tile id="1146" terrain="1,,1,"/>
+ <tile id="1158" terrain=",1,,"/>
+ <tile id="1159" terrain="1,1,,1"/>
+ <tile id="1160" terrain="1,1,1,1"/>
+ <tile id="1161" terrain="1,1,1,"/>
+ <tile id="1162" terrain="1,,,"/>
+ <tile id="1175" terrain=",1,,"/>
+ <tile id="1176" terrain="1,1,,"/>
+ <tile id="1177" terrain="1,,,"/>
+ <tile id="1178" terrain="1,1,,"/>
+ <tile id="1179" terrain="1,1,,"/>
+ <wangsets>
+  <wangset name="New Wang Set" tile="-1">
+   <wangedgecolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangcornercolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangcornercolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0x11111111"/>
+   <wangtile tileid="1" wangid="0x22222222"/>
+   <wangtile tileid="3" wangid="0x21112222"/>
+   <wangtile tileid="4" wangid="0x22211122"/>
+   <wangtile tileid="5" wangid="0x11222221"/>
+   <wangtile tileid="6" wangid="0x22222111"/>
+  </wangset>
+ </wangsets>
 </tileset>
